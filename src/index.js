d99afc99a0add5daa837fcdffac9735dd2dae5ec
GeraSaucedo <jegsg@hotmail.es>
2022-04-06 23:39:51 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
+import { createRoot } from 'react-dom/client';
 import { HeroesApp } from './HeroesApp';
+const container = document.getElementById('root');
+const root = createRoot(container);
 
 
-ReactDOM.render(
-    <HeroesApp />,
-  document.getElementById('root')
-);
+root.render(<HeroesApp />)
